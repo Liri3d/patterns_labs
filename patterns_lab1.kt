1d80130a886b82f7d746b21658544da33142a08e
Liri3d <vlad.ogon.2004@gmail.com>
2024-09-29 19:13:20 +0300

    L1_E5 Аргументы в форме ХЭШа

--- patterns_lab1.kt
+++ patterns_lab1.kt
@@ -11,15 +11,15 @@ class Student(
 {
     // Вторичные конструкторы
     constructor(id: Int, lastName: String, firstName: String, fatherName: String) : this(id, lastName, firstName, fatherName, "", "", "") {
-         println("Создан студент с id=$id, ФИО: $lastName $firstName $fatherName")
+         // println("Создан студент с id=$id, ФИО: $lastName $firstName $fatherName")
     }
     
     constructor(id: Int, lastName: String, firstName: String, fatherName: String, phone: String) : this(id, lastName, firstName, fatherName, phone, "", "") {
-        println("Создан студент с id=$id, ФИО: $lastName $firstName $fatherName, телефон: $phone")
+        // println("Создан студент с id=$id, ФИО: $lastName $firstName $fatherName, телефон: $phone")
     }
     
     constructor(id: Int, lastName: String, firstName: String, fatherName: String, phone: String, telegram: String) : this(id, lastName, firstName, fatherName, phone, telegram, "") {
-        println("Создан студент с id=$id, ФИО: $lastName $firstName $fatherName, телефон: $phone, телеграм: $telegram")
+        // println("Создан студент с id=$id, ФИО: $lastName $firstName $fatherName, телефон: $phone, телеграм: $telegram")
     }
     
     init {
@@ -57,23 +57,50 @@ class Student(
         get() { return field }
 
     fun printInfo() {
-        println("Id: $id | " +
-                "Фамилия: $firstName | " +
-                "Имя: $lastName | " + 
-                "Отчество: $fatherName | " +
-                "Телефон: $phone | " + 
-                "Телеграм: $telegram | " + 
+        println("\nId: $id\n" +
+                "Фамилия: $firstName\n" +
+                "Имя: $lastName\n" + 
+                "Отчество: $fatherName\n" +
+                "Телефон: $phone\n" + 
+                "Телеграм: $telegram\n" + 
                 "Почта: $email")
     }
 }
 
 fun main() {
-    val st1 = Student(1, "Петр", "Петров", "Петрович", "+79999999991", "@petr", "petr.gmail.com")
-    val st2 = Student(2, "Семен", "Семенов", "Семенович", "+79999999992", "@semen")
-    val st3 = Student(3, "Иван", "Иванов", "Иванович", "+79999999993")
-    val st4 = Student(4, "Егор", "Егоров", "Егорович")
+    val st1 = Student(
+        id = 1, 
+        firstName = "Петр",
+        lastName = "Петров",
+        fatherName = "Петрович",
+        phone = "+7(999kр)999-9991",
+        telegram = "@petr",
+        email = "petr.gmail.com"
+    )
+    
+    val st2 = Student(
+        id = 2,
+        firstName = "Семен", 
+        lastName = "Семенов", 
+        fatherName = "Семенович", 
+        phone = "+7(999)999-9992", 
+        telegram = "@semen")
+    
+    val st3 = Student(
+        id = 3, 
+        firstName = "Иван", 
+        lastName = "Иванов", 
+        fatherName = "Иванович", 
+        phone = "+7(999)999-9993")
+    
+    val st4 = Student(
+        id = 4, 
+        firstName = "Егор", 
+        lastName = "Егоров", 
+        fatherName = "Егорович")
 
     st1.printInfo()
     st2.printInfo()
     st3.printInfo()
+    st4.printInfo()
 }
